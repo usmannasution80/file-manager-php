cb393baf8e0d5ee9400c55fa43a05c8ed6eee32c
Usman Nasution <usmannasution32@gmail.com>
2024-03-11 17:38:18 +0700

    Update index.php. Now we can access all child folder of  in index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+  $root = '/storage/emulated/0';
+  $path = $_SERVER['REQUEST_URI'];
+  $files = scandir($root . $path);
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,6 +10,13 @@
     <meta name="viewport" content="width=device-width"/>
   </head>
   <body>
-    
+    <?php
+      if($files){
+        foreach($files as $file){
+          $file .= is_dir($root . $path . $file) ? '/' : '';
+          echo "<a href=\"$path$file\">$file</a><br/>";
+        }
+      }
+    ?>
   </body>
 </html>
