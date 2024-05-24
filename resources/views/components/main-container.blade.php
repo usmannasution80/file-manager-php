9b0d46d98acc420fa0c2e776d530ceb41768d6cc
Usman Nasution <usmannasution32@gmail.com>
2024-05-24 22:44:29 +0700

    Add MainContainer component

---
+++ resources/views/components/main-container.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width"/>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}"/>
+  </head>
+  <body>
+    <script src="{{asset('bootstrap/js/bootstrap.js')}}"></script>
+  </body>
+</html>
