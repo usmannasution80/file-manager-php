d66ed5e30cbb41065a2dc3fe6f35f91376a6bfbe
Usman Nasution <usmannasution32@gmail.com>
2024-05-27 22:46:28 +0700

    Update user migration and user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+     protected $primaryKey = 'username';
     protected function casts(): array
     {
         return [
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->char('username', 20)->unique();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
