2dee484da975d0bb74106f879d26791d4329330c
Usman Nasution <usmannasution32@gmail.com>
2024-05-29 22:08:11 +0700

    Add artisan command to create admin'

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+  $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+Artisan::command('create_admin {username} {email} {password}', function($username, $email, $password){
+  $user = new User();
+  $user->username = $username;
+  $user->email = $email;
+  $user->password = Hash::make($password);
+  $user->save();
+  echo 'New user created!';
+});
