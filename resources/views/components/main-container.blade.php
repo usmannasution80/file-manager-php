8c44856d275aae04fe440609b6b59038389efc15
Usman Nasution <usmannasution32@gmail.com>
2024-06-24 20:05:30 +0700

    Added font awesome

--- resources/views/components/main-container.blade.php
+++ resources/views/components/main-container.blade.php
@@ -4,6 +4,9 @@
     <meta name="viewport" content="width=device-width"/>
     <meta charset="UTF-8"/>
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}"/>
+    <link rel="stylesheet" href="{{asset('fontawesome/css/fontawesome.css')}}"/>
+    <link rel="stylesheet" href="{{asset('fontawesome/css/brands.css')}}"/>
+    <link rel="stylesheet" href="{{asset('fontawesome/css/solid.css')}}"/>
   </head>
   <body class="bg-secondary">
     {{ $slot }}
