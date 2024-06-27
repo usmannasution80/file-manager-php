69f954553a338382f9dd5636de03aaca509d14a5
Usman Nasution <usmannasution32@gmail.com>
2024-06-27 10:26:43 +0700

    Update main header prevent new line in navigation links

--- resources/views/components/main-header.blade.php
+++ resources/views/components/main-header.blade.php
@@ -1,16 +1,45 @@
 <div class="card-header">
-  @foreach($links as $link)
-    <a href="{{$link}}" class="btn">
-      @if($loop->first)
-        <i class="fa-solid fa-house"></i>
-      @else
-        <b>
-          {{preg_replace('/^.*\\//i', '', $link)}}
-        </b>
+  <div id="links" style="white-space: nowrap">
+    @foreach($links as $link)
+      <a href="{{$link}}" class="btn">
+        @if($loop->first)
+          <i class="fa-solid fa-house"></i>
+        @else
+          <b>
+            {{preg_replace('/^.*\\//i', '', $link)}}
+          </b>
+        @endif
+      </a>
+      @if(!$loop->last)
+        <i class="fa-solid fa-chevron-right"></i>
       @endif
-    </a>
-    @if(!$loop->last)
-      <i class="fa-solid fa-chevron-right"></i>
-    @endif
-  @endforeach
+    @endforeach
+  </div>
 </div>
+<script>
+
+  let links = document.getElementById('links');
+  let linkAnchors = links.getElementsByTagName('a');
+  let isThereAnchorMissing = false;
+
+  while(links.offsetWidth < links.scrollWidth){
+    if(linkAnchors.length === 3)
+      break;
+    alert(linkAnchors.length)
+    let index = Math.floor(linkAnchors.length / 2);
+    linkAnchors[index].nextElementSibling.remove();
+    linkAnchors[index].remove();
+    isThereAnchorMissing = true;
+  }
+
+  if(isThereAnchorMissing){
+    let index = Math.floor(linkAnchors.length / 2);
+    let span = document.createElement('span');
+    span.innerHTML = '...';
+    span.classList.add('btn');
+    linkAnchors[index].after(span);
+    linkAnchors[index].remove();
+  }
+
+  links.style.overflow = 'hidden';
+</script>
