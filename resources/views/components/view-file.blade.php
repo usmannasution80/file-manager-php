f079175b1012406de830e23fdd3d59ba16d422c5
Usman Nasution <usmannasution32@gmail.com>
2024-06-29 10:53:05 +0700

    Add download button to view file page

--- resources/views/components/view-file.blade.php
+++ resources/views/components/view-file.blade.php
@@ -1,7 +1,9 @@
 @if($is_file)
 <div class="card-body">
   @if(preg_match('/audio/i', $type))
-    <audio src="{{$src}}" controls/>
+    <audio controls>
+      <source src="{{$src}}"/>
+    </audio>
   @elseif(preg_match('/video/i', $type))
     <video width="100%" controls>
       <source src="{{$src}}"/>
@@ -11,5 +13,11 @@
   @else
     <span>File is not supported.</span>
   @endif
+  <div style="text-align: center">
+    <a href="{{$src}}" download class="btn btn-primary">
+      <i class="fa-solid fa-download"></i>
+      <span>Download</span>
+    </a>
+  </div>
 </div>
 @endif
