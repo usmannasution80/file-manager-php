145c7f0a96cb4e35bc7ca318ec852295a31d78bb
Usman Nasution <usmannasution32@gmail.com>
2024-07-06 22:57:13 +0700

    Some bug fix and add setting button on main header.

--- resources/views/components/main-header.blade.php
+++ resources/views/components/main-header.blade.php
@@ -1,19 +1,36 @@
-<div class="card-header">
-  <div id="header-links">
-    @foreach($links as $link)
-      <a href="{{$link}}">
-        @if($loop->first)
-          <i class="fa-solid fa-house"></i>
-        @else
-          <b>
-            {{preg_replace('/^.*\\//i', '', urldecode($link))}}
-          </b>
+<div id="header" class="card-header container">
+  <div class="row align-items-center">
+    <div class="col" id="header-links">
+      @foreach($links as $link)
+        <a href="{{$link}}">
+          @if($loop->first)
+            <i class="fa-solid fa-house"></i>
+          @else
+            <b>
+              {{preg_replace('/^.*\\//i', '', urldecode($link))}}
+            </b>
+          @endif
+        </a>
+        @if(!$loop->last)
+          <i class="fa-solid fa-chevron-right"></i>
         @endif
-      </a>
-      @if(!$loop->last)
-        <i class="fa-solid fa-chevron-right"></i>
-      @endif
-    @endforeach
+      @endforeach
+    </div>
+    <div id="header-setting-button">
+      <x-popup-menu>
+        <x-slot:button>
+          <button class="btn btn-primary">
+            <i class="fa-solid fa-gear"></i>
+          </button>
+        </x-slot:button>
+        <x-slot:content>
+          <span data-bs-toggle="modal" data-bs-target="#upload">
+            <i class="fa-solid fa-file-arrow-up"></i>
+            <span>Upload</span>
+          </span>
+        </x-slot:content>
+      </x-popup-menu>
+    </div>
   </div>
 </div>
 <script>
