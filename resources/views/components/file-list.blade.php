f4205ff15a08677ab7bf2c77b0a9d9eef5bd443d
Usman Nasution <usmannasution32@gmail.com>
2024-08-03 21:33:57 +0700

    Bug fix when file name contain + character'

--- resources/views/components/file-list.blade.php
+++ resources/views/components/file-list.blade.php
@@ -1,7 +1,7 @@
 <ul class="list-group list-group-flush">
   @foreach($files as $file => $icon)
     <li class="list-group-item" style="overflow: hidden">
-      <a style="text-decoration: none; color: black" href="{{url(preg_replace('/\\/*$/i', '', $_SERVER['REQUEST_URI']) . '/' . $file)}}">
+      <a style="text-decoration: none; color: black" href="{{url(preg_replace('/\\/*$/i', '', $_SERVER['REQUEST_URI']) . '/' . urlencode($file))}}">
         <table border="0">
           <tr>
             <td style="padding-right: 10px">
