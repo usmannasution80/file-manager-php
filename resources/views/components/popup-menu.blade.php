4defe06dc0786028e8a3cd0eec51a1cb9dea23dc
Usman Nasution <usmannasution32@gmail.com>
2024-08-21 09:40:30 +0700

    Set text alignment for popup menu to left, the previous was center

--- resources/views/components/popup-menu.blade.php
+++ resources/views/components/popup-menu.blade.php
@@ -3,7 +3,7 @@
     {{$button}}
   </span>
   <div class="card popup-menu-content" style="z-index:999">
-    <ul class="list-group list-group-flush">
+    <ul class="list-group list-group-flush text-start">
       {{$content}}
     </ul>
   </div>
